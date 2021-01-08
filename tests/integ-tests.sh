dddbdf795540934c89fa23787bc816b2c56882ed
NotDaisuke <notdaisuke@notdaisuke.com>
2021-01-08 04:26:55 -0800

    Test

---
+++ tests/integ-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl localhost:6749/tracery?url=https://s3.amazonaws.com/oddly.shaped.dog/NB.txt
+curl localhost:6749/build-amazon-url?tag=oddlyshapeddog-20&url=https://smile.amazon.com/dp/B000SEGUDE
+
